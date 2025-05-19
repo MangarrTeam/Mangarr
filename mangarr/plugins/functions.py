a6d5759c8674c7bac7a7c33b77087dba4d7efd01
Makhuta <matykubla@gmail.com>
2025-05-20 00:06:34 +0200

    Add
    - some getters

--- mangarr/plugins/functions.py
+++ mangarr/plugins/functions.py
@@ -9,6 +9,16 @@ def load_metadata():
     with open(PLUGINS_METADATA_PATH) as f:
         return json.load(f)
     
+def get_plugin_name(category: str, domain: str) -> str:
+    metadatas = load_metadata()
+    for metadata in metadatas:
+        c = metadata.get("category")
+        d = metadata.get("domain")
+        if c is not None and d is not None and c == category and d == domain:
+            name = metadata.get("name")
+            return name if name is not None else d
+    return domain
+    
 def get_plugin(category: str, domain: str) -> type[MangaPluginBase]:
     key = f"{category}_{domain}"
     if key in PLUGIN_REGISTRY:
@@ -16,3 +26,21 @@ def get_plugin(category: str, domain: str) -> type[MangaPluginBase]:
     
     logger.error(f"Can't fint plugin with key {key}")
     return MangaPluginBase
+
+def get_plugins_domains(category: str) -> list:
+    output = set()
+    for key, _ in PLUGIN_REGISTRY.items():
+        if key.startswith(category):
+            output.add(key.removeprefix(f"{category}_"))
+    return list(output)
+
+def get_plugins() -> list:
+    output = []
+    for key, plugin in PLUGIN_REGISTRY.items():
+        if key.startswith("core"):
+            domain = key.removeprefix("core_")
+            output.append(("core", domain, get_plugin_name("core", domain), sorted(plugin.get_languages())))
+        if key.startswith("community"):
+            domain = key.removeprefix("community_")
+            output.append(("community", domain, get_plugin_name("community", domain), sorted(plugin.get_languages())))
+    return output
