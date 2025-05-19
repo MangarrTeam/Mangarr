3f97d30158e26a7a66e04f73240d4cd2bb2793ef
Makhuta <matykubla@gmail.com>
2025-05-19 03:39:11 +0200

    Add
    - plugin getter

--- mangarr/plugins/functions.py
+++ mangarr/plugins/functions.py
@@ -1,6 +1,18 @@
 import json
-from server.settings import PLUGINS_METADATA_PATH
+from server.settings import PLUGINS_METADATA_PATH, PLUGIN_REGISTRY
+from .base import MangaPluginBase
+import logging
+logger = logging.getLogger(__name__)
+
 
 def load_metadata():
     with open(PLUGINS_METADATA_PATH) as f:
         return json.load(f)
+    
+def get_plugin(category: str, domain: str) -> type[MangaPluginBase]:
+    key = f"{category}_{domain}"
+    if key in PLUGIN_REGISTRY:
+        return PLUGIN_REGISTRY[key]
+    
+    logger.error(f"Can't fint plugin with key {key}")
+    return MangaPluginBase
