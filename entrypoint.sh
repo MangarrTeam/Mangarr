b9100e0a18e7e7b1dadb1ee670adb0683fd86d11
Makhuta <matykubla@gmail.com>
2025-05-22 22:36:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-python manage.py makemigrations database processes frontend api
+python manage.py makemigrations plugins database_other database_data_types database_manga database_users processes frontend api
 python manage.py migrate
-python manage.py makemigrations database processes frontend api
+python manage.py makemigrations plugins database_other database_data_types database_manga database_users processes frontend api
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py make-messages -a
 django-admin compilemessages
-gunicorn --bind 0.0.0.0:8000 server.wsgi:application --reload &
+gunicorn --bind 0.0.0.0:8000 server.wsgi:application --workers 4 --threads 3 --reload &
 
 nginx -g 'daemon off;'
