10aaa8552c612264579b3d0db1de4ae0e9a4558a
Makhuta <matykubla@gmail.com>
2025-05-24 21:34:03 +0200

    Fix
    - now uses cache instead of Consumer variable

--- mangarr/websockets/consumers.py
+++ mangarr/websockets/consumers.py
@@ -2,6 +2,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 import json
 import copy
+from django.core.cache import cache
 
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -30,11 +31,8 @@ def deep_merge(dict_a, dict_b):
             result[key] = copy.deepcopy(value)
     return result
 
-
-class ProcessesConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.group_name = "processes_updates"
-        self.datas = {
+def default_datas() -> dict:
+    return {
             "scanning": {
                 "manga": 0,
                 "chapters": 0
@@ -45,11 +43,15 @@ class ProcessesConsumer(AsyncWebsocketConsumer):
             }
         }
 
+class ProcessesConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user_id = self.scope["user"].id
+        self.group_name = "processes_updates"
+
         await self.channel_layer.group_add(self.group_name, self.channel_name)        
         await self.accept()
 
-        data = await self.get_data_from_db()
-        await self.send(text_data=json.dumps({"message": "Connected!", "data": data}))
+        await self.send(text_data=json.dumps({"message": "Connected!", "data": default_datas()}))
 
     async def disconnect(self, code):
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
@@ -58,11 +60,7 @@ class ProcessesConsumer(AsyncWebsocketConsumer):
         pass
 
     async def send_update(self, event):
-        self.datas = deep_merge(self.datas, event["data"])
+        data = deep_merge(await sync_to_async(cache.get)(f'process_data_{self.user_id}') or default_datas(), event["data"])
+        await sync_to_async(cache.set)(f'process_data_{self.user_id}', data)
 
-        await self.send(text_data=json.dumps(self.datas))
-
-
-    @sync_to_async
-    def get_data_from_db(self):
-        return self.datas
+        await self.send(text_data=json.dumps(data))
